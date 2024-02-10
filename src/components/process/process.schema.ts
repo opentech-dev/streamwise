2890d6c42d9efb59bbd273c719cb32a0ebef7916
lm0x86 <liviu.maftuleac@opentech.dev>
2024-02-10 13:05:51 +0200

    - added defaultQueueOptions for proceses

--- src/components/process/process.schema.ts
+++ src/components/process/process.schema.ts
@@ -1,9 +1,19 @@
+import { QueueOptions } from "bullmq";
 import { FilterSchema } from "../filter/filter.schema";
 import { MergerSchema } from "../merger/merger.schema";
 import { OperationSchema } from "../operation/operation.schema";
+import { JobOptions } from "bull";
 
 export type SchemaType = "process" | "filter" | "operation" | "merger";
 export type ProcessComponents = FilterSchema | OperationSchema | MergerSchema;
+interface qOptions {
+  defaultJobOptions : JobOptions
+  streams?: {
+    events: {
+        maxLen: number;
+    };
+  };
+}
 
 export interface ProcessSchema {
   id: number | string
@@ -11,5 +21,6 @@ export interface ProcessSchema {
   type: "process"
   inbound: string,
   outbound?: string,
-  components: Array<ProcessComponents>
+  components: Array<ProcessComponents>,
+  defaultQueueOptions?: qOptions
 }
--- src/core/bull-wrapper.ts
+++ src/core/bull-wrapper.ts
@@ -1,17 +1,37 @@
-import { Processor, Queue, Worker } from "bullmq";
+import { Processor, Queue, QueueOptions, RedisConnection, Worker } from "bullmq";
 import { DriverConfig } from "@app/types/connection";
+import { JobOptions } from "bull";
 
 export abstract class BullWrapper {
   public driverConfig: DriverConfig;
   public queues: Map<string, Queue> = new Map()
   public workers: Map<string, Worker> = new Map()
-
-  constructor(driverConfig: DriverConfig) {
-    this.driverConfig = driverConfig;
+  private defaultQueOptions: QueueOptions = {
+    // defaultJobOptions: {
+    //   attempts: 3,
+    //   backoff: {
+    //     type: 'exponential',
+    //     delay: 2000,
+    //   },
+    // }
   }
 
-  createQ(queId: string, name: string):Queue {
-    const q = new Queue(name, this.driverConfig)
+  constructor(driverConfig: DriverConfig, defaultQueueOptions?: QueueOptions) {
+    this.driverConfig = driverConfig;
+    if (defaultQueueOptions) {
+      this.defaultQueOptions = defaultQueueOptions
+    }
+  }
+
+  createQ(queId: string, name: string, qOptions?: QueueOptions): Queue {
+    // configure connection
+    const driverConfig = this.driverConfig as unknown as typeof RedisConnection;
+
+    // configure default options
+    // TODO :: include defaultQueOptions support for each component
+    const opts =  qOptions || this.defaultQueOptions;
+
+    const q = new Queue(name, opts, driverConfig)
     console.log('Q --', name);
     this.queues.set(queId, q);
     return q;
